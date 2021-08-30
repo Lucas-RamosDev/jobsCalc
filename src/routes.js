969d3a9936a37d3d9b14e74666e3d77e31b37d7c
Lucas-RamosDev <lucas.bruneli.ramos@gmail.com>
2021-08-30 16:13:03 -0300

    update class

--- src/routes.js
+++ src/routes.js
@@ -5,17 +5,34 @@ const views = __dirname + "/views/"
 
 const profile = {
   name: "Lucas Ramos",
-  avatar: "https://avatars.githubusercontent.com/u/85357089?v=4",
+  avatar: "https://github.com/Lucas-RamosDev.png",
   "monthly-budget": 3000,
   "days-per-week": 5,
   "hours-per-day": 5,
   "vacation-per-year": 4
 }
 
+
+const jobs = [ ]
+
+
 //req, res (pedidos e respostas)
 routes.get('/', (req, res) => res.render(views + "index"))
+
 routes.get('/job', (req, res) => res.render(views + "job"))
+
+// -- ROTA JOBS
+routes.post('/job', (req, res) => {
+  //.push -> esta jogando as informações do "req.body" na constant "jobs"
+  //"req.body" -> esta pegando as informações através do POST vindo do form "form-job"
+  jobs.push(req.body)
+  //console.log(jobs)
+  return res.redirect('/')
+  
+})
+
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
+
 routes.get('/profile', (req, res) => res.render(views + "profile", { profile: profile }))
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -9,9 +9,12 @@ server.set('view engine', 'ejs')
 //habilitar arquivos statics
 server.use(express.static("public"))
 
+// habilitar/usar o "req.body"
+server.use(express.urlencoded({ extended:true }))
+
 //routes
 server.use(routes)
 
 server.listen(3000, () => console.log('Server On'))
 
-// IMPORTANTE - para startar o server execute "npm run dev"
+// IMPORTANTE - para startar o server execute "npm start"
