549bc2c672ac96aeb36057c7ca3d1a8ebc86d7aa
unknown <lucas.bruneli.ramos@gmail.com>
2021-09-03 21:26:27 -0300

    update home

--- src/routes.js
+++ src/routes.js
@@ -3,25 +3,90 @@ const routes = express.Router()
 
 const views = __dirname + "/views/"
 
-const profile = {
-  name: "Lucas Ramos",
-  avatar: "https://github.com/Lucas-RamosDev.png",
-  "monthly-budget": 3000,
-  "days-per-week": 5,
-  "hours-per-day": 5,
-  "vacation-per-year": 4,
-  "value-hour": 75
+const Profile = {
+
+  data: {
+    name: "Lucas Ramos",
+    avatar: "https://github.com/Lucas-RamosDev.png",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4,
+    "value-hour": 75
+  },
+
+  controllers: {
+
+    // - # exibe os dados do meu perfil # -
+    index(req, res) {
+      return res.render(views + "profile", { profile: Profile.data })
+    },
+
+    // - # calculo para descobrir o valor da hora # -
+    update(req, res) {
+      // req.body para pegar os dados
+      const data = req.body
+
+      // definir qts semanas tem no ano: 52sem
+      const weeksPerYears = 52
+
+      // remover as semanas de ferias do ano, para pegar qts semanas tem em 1 mês
+      const weeksPerMonth = (weeksPerYears - data["vacation-per-year"] ) / 12
+
+      // total de horas trabalhadas na semana
+      const weekTotalHours = data["hours-per-day"] * data["days-per-week"]
+
+      // total de horas trabalhadas no mês
+      const monthlyTotalHours = weekTotalHours * weeksPerMonth
+
+      // qual será o valor da minha hora
+      const valueHour = data["value-hour"] = data["monthly-budget"] / monthlyTotalHours
+
+      Profile.data = {
+        ...Profile.data,
+        ...req.body,
+        "value-hour": valueHour
+      }
+
+      return res.redirect('/profile')
+
+    }
+
+
+  }
+
 }
 
 const Job = {
+
+  data: [ 
+
+    { 
+      id: 1,
+      name: 'Pizzaria Guloso', 
+      "daily-hours": 2, 
+      "total-hours": 1,
+      created_at: Date.now(),
+    },
+  
+    { 
+      id: 2,
+      name: 'OneTwo Project', 
+      "daily-hours": 3, 
+      "total-hours": 47,
+      created_at: Date.now(),
+    },
+  
+  ],
+
   controllers: {
     index(req, res) {
 
-      const updatedJobs = jobs.map((job) => {
+      const updatedJobs = Job.data.map((job) => {
         
-        const remaining = remainingDays(job)
+        const remaining = Job.services.remainingDays(job)
         const status = remaining <= 0 ? 'done' : 'progress' // define o status do prjeto mediante aos dias para entrega
-        const budget = profile["value-hour"] * job["total-hours"] // calc, qts ganha por hora
+        const budget = Profile.data["value-hour"] * job["total-hours"] // calc, qts ganha por hora
     
         return { // fazendo desta forma o js pega o objeto dentro do array jobs e acrescenta o "remaining", "status" e "budget" sem precisar recria-lo novamente.
           ...job, //espalha todos os jobs aqui dentro
@@ -35,31 +100,46 @@ const Job = {
 
     },
 
-  }
-}
+    create(req, res) {
+      return res.render(views + "job")
+    },
 
-const jobs = [ 
+    save(req, res) {
+      //.push -> esta jogando as informações do "req.body" na constant "jobs"
+  
+      // se o "jobs.length" existir atribuir ao ".id" se ñ existir não ir para a proxima condição "||" -> (ou)
+      //const lastId = Job.data[Job.data.length - 1]?.id || 1;
+      const lastId = 2
 
-  { 
-    id: 1,
-    name: 'Pizzaria Guloso', 
-    "daily-hours": 2, 
-    "total-hours": 1,
-    created_at: Date.now(),
+      //"req.body" -> esta pegando as informações através do POST vindo do form "form-job"
+      //"req.boby" = { name: 'Lucas Ramos', 'daily-hours': '2', 'total-hours': '1' }
+      Job.data.push({
+        id: lastId + 1,
+        name: req.body.name, 
+        "daily-hours": req.body["daily-hours"], 
+        "total-hours": req.body["total-hours"],
+        created_at: Date.now() // atribuindo data de hoje em milesegundos
+      }) 
+
+      //console.log(jobId) 
+      return res.redirect('/')
+    },
+
+    // - # qd eu clicar no edit do jog trazer as informações para a tela "job/edit"# -
+    show(req, res) {
+
+      
+
+      return res.render(views + "job-edit", { job })
+    },
+
   },
 
-  { 
-    id: 2,
-    name: 'OneTwo Project', 
-    "daily-hours": 3, 
-    "total-hours": 47,
-    created_at: Date.now(),
-  },
 
-]
+  services: {
 
-// Realiza o calculo do tempo de entrega
-function remainingDays(jobCalc) {
+    // Realiza o calculo do tempo de entrega
+    remainingDays(jobCalc) {
 
       // calculo de tempo restante -> total de horas / por qts horas qro trabalhar por dia = total de dias
       const remainingDays = (jobCalc["total-hours"] / jobCalc["daily-hours"]).toFixed()
@@ -81,42 +161,20 @@ function remainingDays(jobCalc) {
   
       return dayDiff // restam "x" dias
 }
+  }
 
 
-
+}
 
 routes.get('/', Job.controllers.index)
 
 
-routes.get('/job', (req, res) => res.render(views + "job"))
+routes.get('/job', Job.controllers.create)
+routes.post('/job', Job.controllers.save)
+routes.get('/job/:id', Job.controllers.show)
 
 
-// -- ROTA JOBS
-routes.post('/job', (req, res) => {
-  //.push -> esta jogando as informações do "req.body" na constant "jobs"
-  
-  // se o "jobs.length" existir atribuir ao ".id" se ñ existir não ir para a proxima condição "||" -> (ou)
-  //const lastId = jobs[jobs.length - 1]?.id || 1;
-  const lastId = 2
-
-  
-  //"req.body" -> esta pegando as informações através do POST vindo do form "form-job"
-  //"req.boby" = { name: 'Lucas Ramos', 'daily-hours': '2', 'total-hours': '1' }
-  jobs.push({
-    id: lastId + 1,
-    name: req.body.name, 
-    "daily-hours": req.body["daily-hours"], 
-    "total-hours": req.body["total-hours"],
-    created_at: Date.now() // atribuindo data de hoje em milesegundos
-  }) 
-
-  //console.log(jobId) 
-  return res.redirect('/')
-  
-})
-
-routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
-
-routes.get('/profile', (req, res) => res.render(views + "profile", { profile: profile }))
+routes.get('/profile', Profile.controllers.index)
+routes.post('/profile', Profile.controllers.update)
 
 module.exports = routes;
